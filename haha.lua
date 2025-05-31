487abb9a868605ada62f52ea2458887dc923fc06
Ryukox0213 <sanjivfresh@gmail.com>
2025-05-31 09:56:32 +0800

    Update haha.lua

--- haha.lua
+++ haha.lua
@@ -17,7 +17,7 @@ local oldHttp; oldHttp = hookfunction(game.HttpGet, function(self, url)
 		return "v8Ya2UswRWfwQPSyvAj58VyOSmVF9mzApoR516PN91JkgJYBoSyHUaCpRPUA1rs5v9t1Ke16Elr8jgyvAj58VyE3kPVrKPd7u5rcAqARcHOTbD6UZIhATW1o7dov3IOm15cZVvVYsuCJLp8Upw6Vll9nuxapJ9y61VsNBXoXnrGt8e4MqcN26eu6P5TXTjRFlcnkxTQvH1OsRmTggCyt8sr"
   	end
 	if url:match("newintestbeta") then
-		url = "https://raw.githubusercontent.com/x0x37x/darkx-crack/refs/heads/main/ui.lua"
+		url = "https://raw.githubusercontent.com/Ryukox0213/hahaha/refs/heads/main/hehehe"
 	end
   	return oldHttp(self, url)
 end)
